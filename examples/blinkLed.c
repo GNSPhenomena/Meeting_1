55e58786f6c16d8543a96bdc6b5a5dba9abebe69
Albert <32423607+GNSPhenomena@users.noreply.github.com>
2017-09-30 17:53:49 -0700

    Update blinkLed.c

--- examples/blinkLed.c
+++ examples/blinkLed.c
@@ -1,15 +1,15 @@
 //---------------- Initialization ----------------
 void setup() {
-  // put your setup code here, to run once:
 	pinMode(0,OUTPUT); // Initialize pin 0
 }
 
 //---------------- Main Function ----------------
 void loop() {
-  // put your main code here, to run repeatedly:
+	//Turn on the LED
 	digitalWrite(0,HIGH); // Set pin 0's output state as HIGH
 	delay(1000); // Hold for 1 sec
-
+	
+	//Turn off the LED
 	digitalWrite(0,LOW); // Set pin 0's output state as LOW
 	delay(1000); // Hold for 1 sec
 }
